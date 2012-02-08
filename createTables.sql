14d90d2a5493e5ea0f3b5bafe4f576800c07cc43
tilucsi <theduvn@yahoo.com>
2012-02-08 00:19:58 -0800

    Update some tables. Remove prefer_contact field in grower table.

--- createTables.sql
+++ createTables.sql
@@ -92,6 +92,21 @@ INSERT INTO months (name) VALUES -- May not be necessary, just use 1-12?
 	('November'),
 	('December');
 
+DROP TABLE IF EXISTS days;
+CREATE TABLE days (
+id INT AUTO_INCREMENT PRIMARY KEY,
+name nvarchar(255) NOT NULL
+) ENGINE=innodb;
+
+INSERT INTO days (name) VALUES 
+('Monday'),
+('Tuesday'),
+('Wednesday'),
+('Thursday'),
+('Friday'),
+('Saturday'),
+('Sunday');
+
 
 DROP TABLE IF EXISTS sources;	
 CREATE TABLE sources (
@@ -117,8 +132,7 @@ CREATE TABLE growers (
 	first_name	VARCHAR(255) NOT NULL,
 	last_name	VARCHAR(255) NOT NULL,
 	phone		VARCHAR(17) NOT NULL, -- maybe (http://stackoverflow.com/q/75105/266535)
-	email		VARCHAR(255) NOT NULL, -- max is actually 320, but so rare
-	prefer_contact CHAR(1), -- P for phone, E for email
+	email		VARCHAR(255) NOT NULL, -- max is actually 320, but so rare	
 	street		VARCHAR(255) NOT NULL,
 	city		VARCHAR(255) NOT NULL,
 	state		CHAR(2)		NOT NULL, -- this makes sense right?
@@ -133,30 +147,30 @@ CREATE TABLE growers (
 ) ENGINE=innodb;
 
 -- start temp insert (for debugging front end)
-INSERT INTO growers (first_name, last_name, phone, email, prefer_contact, street, city, state, zip, property_type_id, property_relationship_id) VALUES
-('Steven', 'Sommers', '(949) 334-1234', 'sommers@uci.edu', 'P', '123 Fake St', 'Irvine', 'CA', '91234', 1, 2),
-('Lawrence', 'Nanners', '(949) 633-1234', 'nanners@uci.edu', 'P', '313 Fake St', 'Laguna', 'CA', '97234', 3, 2),
-('Fernando', 'Vegas', '(949) 514-1234', 'vegas@uci.edu', 'E', '23 Fake St', 'Irvine', 'CA', '91234', 3, 1),
-('Fernanda', 'Vargas', '(949) 533-1234', 'vargas@aol.com', 'E', '23 Real St', 'Irvine', 'CA', '93731', 3, 1),
-('Billy', 'Bob', '(800) 555-1234', 'bob@aol.com', 'E', '123 Spooner St', 'Springfield', 'IL', '65134', 4, 4)
+INSERT INTO growers (first_name, last_name, phone, email, street, city, state, zip, property_type_id, property_relationship_id) VALUES
+('Steven', 'Sommers', '(949) 334-1234', 'sommers@uci.edu','123 Fake St', 'Irvine', 'CA', '91234', 1, 2),
+('Lawrence', 'Nanners', '(949) 633-1234', 'nanners@uci.edu', '313 Fake St', 'Laguna', 'CA', '97234', 3, 2),
+('Fernando', 'Vegas', '(949) 514-1234', 'vegas@uci.edu', '23 Fake St', 'Irvine', 'CA', '91234', 3, 1),
+('Fernanda', 'Vargas', '(949) 533-1234', 'vargas@aol.com', '23 Real St', 'Irvine', 'CA', '93731', 3, 1),
+('Billy', 'Bob', '(800) 555-1234', 'bob@aol.com', '123 Spooner St', 'Springfield', 'IL', '65134', 4, 4)
 ;
 -- end temp insert
 
 
 
-DROP TABLE IF EXISTS trees;
-CREATE TABLE trees (
-	id 		INT AUTO_INCREMENT PRIMARY KEY,
-	tree_type 	INT NOT NULL FOREIGN KEY REFERENCES tree_types(id),
-	amount 		INT NOT NULL,
-	height 		INT NOT NULL FOREIGN KEY REFERENCES heights(id),
-	ripe_month 	INT NOT NULL FOREIGN KEY REFERENCES months(id), -- May not be necessary: Use 1-12
-	issues 		VARCHAR(64) DEFAULT "No",
-	pruned 		VARCHAR(64) DEFAULT "No",
-	sprayed 	VARCHAR(64) DEFAULT "No",
-	notes 		TEXT,
-	grower_id	INT NOT NULL FOREIGN KEY REFERENCES growers(id)
-) ENGINE=innodb;
+--DROP TABLE IF EXISTS trees;
+--CREATE TABLE trees (
+--	id 		INT AUTO_INCREMENT PRIMARY KEY,
+--	tree_type 	INT NOT NULL FOREIGN KEY REFERENCES tree_types(id),
+--	amount 		INT NOT NULL,
+--	height 		INT NOT NULL FOREIGN KEY REFERENCES heights(id),
+--	ripe_month 	INT NOT NULL FOREIGN KEY REFERENCES months(id), -- May not be necessary: Use 1-12
+--	issues 		VARCHAR(64) DEFAULT "No",
+--	pruned 		VARCHAR(64) DEFAULT "No",
+--	sprayed 	VARCHAR(64) DEFAULT "No",
+--	notes 		TEXT,
+--	grower_id	INT NOT NULL FOREIGN KEY REFERENCES growers(id)
+--) ENGINE=innodb;
 
 
 
@@ -166,17 +180,179 @@ CREATE TABLE trees (
 -- TODO: privileges table
 -- TODO: distribution_sites table
 
+--Based on the registration form, we should group all tree type into 1 record
+DROP TABLE IF EXISTS grower_tree;
+CREATE TABLE grower_tree (
+grower_id INT,
+tree_id INT,
+number INT,
+avgHeight_id INT, 
+sprayed TINYINT NULL, --1 Yes -- 0 No
+pruned TINYINT NULL,  --1 Yes -- 0 No
+diseased TINYINT NULL,--1 Yes -- 0 No
+notes 		TEXT,
+CONSTRAINT fk_grower FOREIGN KEY (grower_id) REFERENCES growers(id),
+CONSTRAINT fk_tree FOREIGN KEY (tree_id) REFERENCES tree_types(id),
+CONSTRAINT fk_height FOREIGN KEY (avgHeight_id) REFERENCES tree_heights(id)
+) ENGINE=innodb;
+
+
+INSERT INTO grower_tree (grower_id, tree_id, number,avgHeight_id, sprayed, pruned, diseased) VALUES
+(1, 2, 5, 2, 0, 0, 0), 
+(2, 3, 10, 5, 0, 1, 0)
+;
+
+DROP TABLE IF EXISTS month_harvest;
+CREATE TABLE month_harvest (
+grower_id INT,
+tree_type_id INT,
+month_id INT,
+CONSTRAINT pk_month_harvest PRIMARY KEY (grower_id, tree_type_id, month_id),
+CONSTRAINT fk_grower_id FOREIGN KEY (grower_id) REFERENCES growers(id),
+CONSTRAINT fk_tree_type_id FOREIGN KEY (tree_type_id) REFERENCES tree_types(id),
+CONSTRAINT fk_month_id FOREIGN KEY (month_id) REFERENCES months(id)
+) ENGINE=innodb;
+
+INSERT INTO month_harvest (grower_id, tree_type_id, month_id) VALUES
+(1, 2, 4),
+(1, 2, 9),
+(2, 3, 7)
+;
+
+DROP TABLE IF EXISTS volunteer_type;
+CREATE TABLE volunteer_type (
+id INT AUTO_INCREMENT PRIMARY KEY,
+type nvarchar(255) NOT NULL
+) ENGINE=innodb;
+
+INSERT INTO volunteer_type (type) VALUES
+('Harvester'),
+('Harvest Captain '),
+('Driver '),
+('Ambassador'),
+('Tree Scout');
+
+
+DROP TABLE IF EXISTS volunteers;
+CREATE TABLE volunteers (
+id INT AUTO_INCREMENT PRIMARY KEY,
+first_name nvarchar(255) NOT NULL,
+middle_name nvarchar(255) NULL,
+last_name nvarchar(255) NOT NULL,
+phone nvarchar(17) NOT NULL, 
+email nvarchar(255) NOT NULL, 
+active TINYINT, -- 1-Active, 0-- Inactive
+street nvarchar(255) NOT NULL,
+city nvarchar(255) NOT NULL,
+state CHAR(2) NOT NULL, 
+zip nvarchar(5) NOT NULL 
+) ENGINE=innodb;
+
+
+INSERT INTO volunteers (first_name, middle_name, last_name, phone, email, active, street, city, state, zip) VALUES
+('Du','The', 'Du', '(123) 456-7890', 'dtdu@uci.edu', 1, '456 Fake St', 'Irvine', 'CA', '91234')
+;
+
+--A volunteer can have many rolls
+DROP TABLE IF EXISTS volunteer_roll;
+CREATE TABLE volunteer_roll (
+volunteer_id INT NOT NULL,
+volunteer_type_id INT NOT NULL,
+CONSTRAINT pk_volunteer_roll PRIMARY KEY (volunteer_id, volunteer_type_id),
+CONSTRAINT fk_volunteer_id FOREIGN KEY (volunteer_id) REFERENCES volunteers(id),
+CONSTRAINT fk_volunteer_type_id FOREIGN KEY (volunteer_type_id) REFERENCES volunteer_type(id)
+) ENGINE=innodb;
+
+INSERT INTO volunteer_roll(volunteer_id, volunteer_type_id) VALUES
+(1, 2),
+(1, 3)
+;
+
+DROP TABLE IF EXISTS volunteer_prefer;
+CREATE TABLE volunteer_prefer (
+volunteer_id INT NOT NULL,
+day_id INT NOT NULL,
+CONSTRAINT pk_volunteer_prefer PRIMARY KEY (volunteer_id, day_id),
+CONSTRAINT fk_volunteer_prefer_id FOREIGN KEY (volunteer_id) REFERENCES volunteers(id),
+CONSTRAINT fk_day_id FOREIGN KEY (day_id) REFERENCES days(id)
+) ENGINE=innodb;
+
+INSERT INTO volunteer_prefer( volunteer_id, day_id) VALUES
+(1, 2),
+(1, 5);
+
+
+DROP TABLE IF EXISTS events;
+CREATE TABLE events (
+id INT AUTO_INCREMENT PRIMARY KEY,
+name nVARCHAR(255) NOT NULL,
+grower_id INT NOT NULL,
+captain_id INT NOT NULL,
+date datetime,
+CONSTRAINT fk_event_grower_id FOREIGN KEY (grower_id) REFERENCES growers(id),
+CONSTRAINT fk_event_captain_id FOREIGN KEY (captain_id) REFERENCES volunteers(id)
+) ENGINE=innodb;
+
+
+-- This table associated each volunteer with each event.
+DROP TABLE IF EXISTS volunteer_event;
+CREATE TABLE volunteer_event (
+event_id INT NOT NULL,
+volunteer_id INT NOT NULL,
+driver TINYINT NOT NULL,
+CONSTRAINT pk_volunteer_event PRIMARY KEY (volunteer_id, event_id),
+CONSTRAINT fk_volunteer_event_volunteer_id FOREIGN KEY (volunteer_id) REFERENCES volunteers(id),
+CONSTRAINT fk_volunteer_event_event_id FOREIGN KEY (event_id) REFERENCES events(id)
+) ENGINE=innodb;
+
+DROP TABLE IF EXISTS harvest;
+CREATE TABLE harvest (
+event_id INT NOT NULL,
+tree_id INT NOT NULL,
+pound INT NOT NULL,
+CONSTRAINT pk_harvest PRIMARY KEY (tree_id, event_id),
+CONSTRAINT fk_harvest_tree_id FOREIGN KEY (tree_id) REFERENCES tree_types(id),
+CONSTRAINT fk_harvest_event_id FOREIGN KEY (event_id) REFERENCES events(id)
+) ENGINE=innodb;
+
+DROP TABLE IF EXISTS distribution;
+CREATE TABLE distribution (
+id INT AUTO_INCREMENT PRIMARY KEY,
+name nvarchar(255) NOT NULL,
+phone nvarchar(17) NOT NULL, 
+email nvarchar(255) NOT NULL, 
+street nvarchar(255) NOT NULL,
+city nvarchar(255) NOT NULL,
+state CHAR(2) NOT NULL, 
+zip nvarchar(5) NOT NULL ,
+hours nvarchar(255) NOT NULL
+) ENGINE=innodb;
+
+
+DROP TABLE IF EXISTS driving;
+CREATE TABLE driving (
+event_id INT NOT NULL,
+tree_id INT NOT NULL,
+volunteer_id INT NOT NULL,
+distribution_id INT NOT NULL,
+pound INT NOT NULL,
+CONSTRAINT pk_driving PRIMARY KEY (tree_id, event_id, volunteer_id, distribution_id),
+CONSTRAINT fk_driving_tree_id FOREIGN KEY (tree_id) REFERENCES tree_types(id),
+CONSTRAINT fk_driving_event_id FOREIGN KEY (event_id) REFERENCES events(id),
+CONSTRAINT fk_driving_volunteer_id FOREIGN KEY (volunteer_id) REFERENCES volunteers(id),
+CONSTRAINT fk_driving_distribution_id FOREIGN KEY (distribution_id) REFERENCES distributions(id)
+) ENGINE=innodb;
+
 
 
 DROP TABLE IF EXISTS donations;
 CREATE TABLE donations (
-	id		INT AUTO_INCREMENT PRIMARY KEY,
-	donation	VARCHAR(255) NOT NULL,
-	donor		VARCHAR(255) DEFAULT "Anonymous",
-	value		INT
+id INT AUTO_INCREMENT PRIMARY KEY,
+donation nVARCHAR(255) NOT NULL,
+donor nVARCHAR(255) DEFAULT "Anonymous",
+value double,
+date datetime
 ) ENGINE=innodb;
-
-
 
 -- Please follow naming conventions above (plural table names)
 
