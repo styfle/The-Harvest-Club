ea4ff8b0228b9a6d4bb0a851a7bc65e2bf51fe75
Steven Salat <ssalat@uci.edu>
2012-02-11 19:49:11 -0800

    The demo table now gets volunteers for db using ajax.php file. I also added the Database class for quick queries. Table is also stretched to 90% of screen.

--- ajax.php
+++ ajax.php
@@ -1,21 +1,66 @@
 <?php
 
 require_once('include/Database.inc.php');
+// TODO add authentication
 
-//echo 'CMD: ' . $_REQUEST['cmd'];
+header('Content-type: application/json');
 
 $cmd = $_REQUEST['cmd'];
-$data = array('status'=>200);
+$data = array('status'=>200); // default to OK
+// See http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
 
-if ($cmd == 'page1') {
-	$data['title'] = 'Page 1';
-	$data['content'] = 'Welcome to America!';
-} else if ($cmd == 'page2') {
-	$data['title'] = 'Page 2';
-	$data['content'] = 'Now leaving America!';
+function getError($r) {
+	global $data;
+
+	if ($r->isValid())
+		return 0; // 0 is success
+
+	$data['status'] = 999;
+	$data['message'] = 'DB: ' . $e; // show error message
+	return $data;
+}
+
+function getTable($sql) {
+	global $db;
+	global $data;
+	//$data['datatable'] = array('aoColumns', 'aaData');
+
+	$r = $db->q($sql);
+	if (getError($r))
+		return $data;
 	
+	$a = $r->buildArray();
+
+	foreach ($a[0] as $k => $v) {
+		$column = array();
+		$column['sTitle'] = $k;
+		//$data['datatable']['aoColumns'][]['sTitle'] = $k;
+		if ($k == 'id') {
+			$column['bSearchable'] = false;
+			$column['bVisible'] = false;
+		} else if ($k == 'street') {
+			$column['bVisible'] = false; // hide street
+		}
+		$data['datatable']['aoColumns'][] = $column;
+	}
 	
-	
+	foreach ($a as $v) {
+		$record = array();
+		foreach ($v as $name=>$value) {
+			$record[] = $value;
+		}
+		$data['datatable']['aaData'][] = $record;
+	}
+
+}
+
+if ($cmd == 'home') {
+	$data['title'] = 'Notifications';
+	$data['content'] = '';
+} else if ($cmd == 'volunteers') {
+	$sql = "SELECT * FROM volunteers;";
+	$data['title'] = 'Volunteers';
+	getTable($sql);
 } else if ($cmd == 'get_grower_email') {
 	date_default_timezone_set("America/Los_Angeles");
 	$data['date'] = date('F d, Y');
@@ -26,7 +71,7 @@ if ($cmd == 'page1') {
 	//$data['zip'] = getZip();
 	//$data['fruit'] = getFruit();
 } else {
-	$data['status'] = 404;
+	$data['status'] = 404; // Not found
 	$data['error'] = "Unknown ajax command: $cmd";
 }
 
---
+++ include/Database.inc.php
@@ -0,0 +1,143 @@
+<?php
+// this class requires a config file with defined constants
+// see the bottom of this file for more info
+require_once('include/config.inc.php');
+
+class Database
+{
+	private $_server;
+	private $_user;
+	private $_pass;
+	private $_db;
+	
+	private $_con;
+	private $_result;
+	
+	function __construct($server, $username, $password, $database)
+	{
+		$this->_server = $server;
+		$this->_user = $username;
+		$this->_pass = $password;
+		$this->_db = $database;
+		
+		$this->_con = mysql_connect($this->_server, $this->_user, $this->_pass);
+		if (!$this->_con) {
+			die('Could not connect to database: ' . mysql_error()); // TODO turn this into an exception
+		}
+		mysql_select_db($this->_db, $this->_con);
+	}
+	
+	function q($sql, $params=null)
+	{
+		if ($params) {
+			$sql = vsprintf($sql, $params);
+		}
+		$this->_result = new Result(mysql_query($sql), $this->_con);
+		return $this->_result;
+	}
+	
+	function result()
+	{
+		return $this->_result;
+	}
+	
+	function numAffectedRows()
+	{
+		return mysql_affected_rows($this->_con);
+	}
+	
+	function escapeStr($string)
+	{
+		return mysql_real_escape_string($string, $this->_con);
+	}
+	
+	function getInsertId()
+	{
+		return mysql_insert_id($this->_con);
+	}
+	
+	function listTables()
+	{
+		$this->_result = new Result(@mysql_list_tables($this->_db, $this->_con));
+		return $this->_result;
+	}
+	
+	function listFields($table)
+	{
+		return $this->q("show columns from " . $this->escapeStr($table), $this->_con);
+	}
+	
+	function error()
+	{
+		return mysql_error($this->_con);
+	}
+	
+} // class Database
+
+class Result
+{
+	private $_r;
+	
+	function __construct($result)
+	{
+		$this->_r = $result;
+	}
+	
+	function isValid()
+	{
+		return ($this->_r !== false);
+	}
+	
+	function hasRows()
+	{
+		return ($this->numRows() > 0);
+	}
+	
+	function numRows()
+	{
+		return mysql_num_rows($this->_r);
+	}
+	
+	function numFields()
+	{
+		return mysql_num_fields($this->_r);
+	}
+	
+	function getAssoc()
+	{
+		return mysql_fetch_assoc($this->_r);
+	}
+	
+	function getArray($type = MYSQL_BOTH)
+	{
+		return mysql_fetch_array($this->_r, $type);
+	}
+	
+	function getRow()
+	{
+		return mysql_fetch_row($this->_r);
+	}
+	
+	function free()
+	{
+		return mysql_free($this->_r);
+	}
+	
+	function buildArray()
+	{
+		$a = array();
+		
+		while(($row = mysql_fetch_assoc($this->_r)) !== false) {
+			$a[] = $row; // push into array
+		}
+			
+		return $a;
+	}
+} // class Result
+
+// these constants should be defined in your config file
+// CONSTANTS: hostname, username, password, database name
+// creates a global db object for making queries
+$db = new Database(MYSQL_SERVER, MYSQL_USER, MYSQL_PASS, MYSQL_DB);
+
+?>
